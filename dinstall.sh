75fdf403f89879fd4c751b528a3bcd10672f1aeb
Jacob Neher <services@threeshadowsmedia.com>
2013-11-29 19:50:08 -0700

    More random changes getting ready for script to be usable.

--- dinstall.sh
+++ dinstall.sh
@@ -3,9 +3,10 @@
 ##   VERSION: 0.1
 #######################################################################
 # NOTES
-# The user running this script must be able to access the vhost file!
+# The user running this script must be able to write to the vhost file!
 #
 #######################################################################
+
 ##### CONFIGURABLE DEFAULTS
 # Set the location of the file that holds the template vhost configuration
 vhost_template=/var/www/scripts/dinstall/vhost.txt
@@ -18,6 +19,7 @@ vhost_file=/etc/httpd/vhosts.d/sites.conf
 ##########################################################################################
 
 ##### DEFAULTS
+display_help=0
 user_dest=0
 all_defaults=0
 no_vhost=0
@@ -31,34 +33,36 @@ site_acct=admin
 site_pass=Jd4ms!
 
 
-
 ##### PARSE ARGUMENTS
 while [ "$1" != "" ]; do
   case $1 in
+    -h | --help )
+      display_help=1
+      ;;
     -dbn* | --dbname* )
-      db_name=`expr match "$1" '(?:-dbn|--dbname)=\(.*\)'`
+      db_name=$(expr match "$1" '(?:-dbn|--dbname)=\(.*\)')
       ;;
     -dbu* | --dbuser* )
-      db_user=`expr match "$1" '(?:-dbu|--dbuser)=\(.*\)'`
+      db_user=$(expr match "$1" '(?:-dbu|--dbuser)=\(.*\)')
       ;;
     -dbp* | --dbpass* )
-      db_user=`expr match "$1" '(?:-dbp|--dbpass)=\(.*\)'`
+      db_user=$(expr match "$1" '(?:-dbp|--dbpass)=\(.*\)')
       ;;
     -dm* | --drushmake* )
-      drush_makefile=`expr match "$1" '(?:-dm|--drushmake)="{0,1}\(.*\)"{0,1}'`
+      drush_makefile=$(expr match "$1" '(?:-dm|--drushmake)="{0,1}\(.*\)"{0,1}')
       ;;
     -dest* | --destination* )
-      drupal_dest=`expr match "$1" '(?:-dest|--destination)="{0,1}\(.*\)"{0,1}'`
+      drupal_dest=$(expr match "$1" '(?:-dest|--destination)="{0,1}\(.*\)"{0,1}')
       user_dest=1
       ;;
     -sn* | --site-name* )
-      site_name=`expr match "$1" '(?:-sn|--site-name)="{0,1}\(.*\)"{0,1}'`
+      site_name=$(expr match "$1" '(?:-sn|--site-name)="{0,1}\(.*\)"{0,1}')
       ;;
     -sa* | --site-acct* )
-      site_acct=`expr match "$1" '(?:-sa|--site-acct)=\(.*\)'`
+      site_acct=$(expr match "$1" '(?:-sa|--site-acct)=\(.*\)')
       ;;
     -sp* | --site-pass* )
-      site_pass=`expr match "$1" '(?:-sp|--site-pass)=\(.*\)'`
+      site_pass=$(expr match "$1" '(?:-sp|--site-pass)=\(.*\)')
       ;;
     -nv | --no-vhost )
       no_vhost=1
@@ -71,100 +75,59 @@ while [ "$1" != "" ]; do
 done
 
 
-# GATHER INFORMATION FROM THE USER
-
-# If a --dbname parameter was not passed we need to ask for the name of the database to create
-if [ "$db_name" == "" ]; then
-  read -p "Name of database to create: " db_name
-fi
-# Check the name of the database so that if the database already exists we don't go any further
-check_database_exists
-
-# We need to check to see if the db_user was passed in.
-# If so, we need to make sure that a password was sent in as well.
-if [ "$db_user" != 'root' ]; then
-    db_pass=
-    while [[ $db_pass = "" ]]; do
-      read -p "Password for database user $db_user: " db_pass
-    done
-fi
-
-
-# Check for proper settings for drush make
-### If drupal_dest equals the current directory (.)
-### AND the user has not declared a path using --dest
-### AND the user didn't set the --defaults flag to use all defaults
-if [ "$drupal_dest" == '.' ] && [ "$user_dest" == "0" ] && [ "$all_defaults" == "0" ]; then
-  read -p "You are going to install Drupal in the current directory. Are you sure? (y/n) " yn
-  case $yn in
-    [!yY] )
-      read -p "Enter the full path of where to install Drupal: " drupal_dest
-      ;;
-  esac;
-fi
-
-
-# Items to handle before we can run drush site-install
-if [ "$site_name" == "" ]; then
-  read -p "Enter the human-readable name for this site: " site_name
-fi
-
-
-# Items to handle before we can setup a vhost record
-# Ask for the domain name as long as we are supposed to be setting up a vhost
-if [ "$no_vhost" == "0" ]; then
-  # Prompt the user for the domain to use for the site
-  read -p "Enter the domain name (WITHOUT TLD) for this site: " domain_name
-fi
-
-
-##########################
-
-
-# # CREATE A DATABASE
-# create_database
-# # RUN DRUSH MAKE
-# drupal_download
-# # INSTALL DRUPAL
-# drupal_install
-# CREATE VHOST RECORD
-create_vhost
-
-
-
 ##### FUNCTIONS
 function check_database_exists
 {
   # Assign our test for the given database to a variable
-  database_present=`mysql -uroot -p${db_root_pass} -e "show databases like '$db_name'"`
+  database_present=$(mysql -uroot -p${db_root_pass} -e "show databases like '$db_name'")
   if [[ $database_present ]]; then
     echo "ERROR: A database with the name $db_name already exists."
     # Prompt for a new database name and assign it to the same variable
-    read -p "Enter a new database name: " db_name
+    db_name=
+    while [[ $db_name = "" ]]; do
+      read -p "Enter a new database name: " db_name
+    done
     # Re-run this function to test the newly given database name
     check_database_exists
-    # exit;
+  fi
+}
+
+function check_database_user_exists
+{
+  user_present=$(mysql -uroot -p${db_root_pass} -e "use mysql; SELECT user FROM user WHERE user='$db_user';")
+  if [[ $user_present ]]; then
+    read -p "The user $db_user already exists in the database. Continue using this user? (y/n) " yn
+    case $yn in
+      [!yY] )
+        db_user=
+        while [[ $db_user = "" ]]; do
+          read -p "Name of database user to create: " db_user
+        done
+        # Re-run this function to test the newly given user
+        check_database_user_exists
+        ;;
+    esac;
   fi
 }
 
 function create_database
 {
   sql="CREATE DATABASE IF NOT EXISTS $db_name; GRANT ALL ON $db_name.* TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass'; FLUSH PRIVILEGES;"
-  `which mysql` -uroot -p${db_root_pass} -e "$sql"
+  $(which mysql) -uroot -p${db_root_pass} -e "$sql"
 }
 
 function drupal_download
 {
   echo '*********************************'
   echo 'Drupal is now being downloaded...'
-  `drush make $drush_makefile $drupal_dest`
+  $(drush make $drush_makefile $drupal_dest)
 }
 
 function drupal_install
 {
   echo '********************************'
   echo 'Drupal is now being installed...'
-  `drush site-install $drush_makefile --db-url=mysql://$db_user:$db_pass@localhost/$db_name --site-name="$site_name" --account-name=$site_acct --account-pass=$site_pass`
+  $(drush site-install $drush_makefile --db-url=mysql://$db_user:$db_pass@localhost/$db_name --site-name="$site_name" --account-name=$site_acct --account-pass=$site_pass)
 }
 
 function create_vhost
@@ -192,3 +155,101 @@ function create_vhost
     # su -c'service httpd restart'
   fi 
 }
+
+function display_help
+{
+  echo "This will eventually display help text ... yay!"
+  exit;
+}
+
+
+# CHECK IF HELP IS REQUESTED FIRST THING SO THAT NOTHING ELSE GETS PROCESSED
+if [ "$display_help" == "1" ]; then
+  display_help
+fi
+
+# GATHER INFORMATION FROM THE USER
+
+# If a --dbname parameter was not passed we need to ask for the name of the database to create
+if [ "$db_name" == "" ]; then
+  db_name=
+  while [[ $db_name = "" ]]; do
+    read -p "Name of database to create: " db_name
+  done
+fi
+# Check the name of the database so that if the database already exists we don't go any further
+check_database_exists
+
+# Check to make sure that the user wants to use the root database user
+if [ "$db_user" == "root" ]; then
+  read -p "You are going to use the root database user for this site. Are you sure? (y/n) " yn
+  case $yn in
+    [!yY] )
+      db_user=
+      while [[ $db_user = "" ]]; do
+        read -p "Name of database user: " db_user
+      done
+      check_database_user_exists
+      ;;
+  esac;
+fi
+
+
+# We need to check to see if the db_user was passed in.
+# If so, we need to make sure that a password was sent in as well.
+if [ "$db_user" != 'root' ]; then
+  db_pass=
+  while [[ $db_pass = "" ]]; do
+    read -p "Password for database user $db_user: " db_pass
+  done
+fi
+
+
+# Check for proper settings for drush make
+### If drupal_dest equals the current directory (.)
+### AND the user has not declared a path using --dest
+### AND the user didn't set the --defaults flag to use all defaults
+if [ "$drupal_dest" == '.' ] && [ "$user_dest" == "0" ] && [ "$all_defaults" == "0" ]; then
+  read -p "You are going to install Drupal in the current directory. Are you sure? (y/n) " yn
+  case $yn in
+    [!yY] )
+      drupal_dest=
+      while [[ $drupal_dest = "" ]]; do
+        read -p "Enter the full path of where to install Drupal: " drupal_dest
+      done
+      ;;
+  esac;
+fi
+
+
+# Items to handle before we can run drush site-install
+if [ "$site_name" == "" ]; then
+  site_name=
+  while [[ $site_name = "" ]]; do
+    read -p "Enter the human-readable name for this site: " site_name
+  done
+fi
+
+
+# Items to handle before we can setup a vhost record
+# Ask for the domain name as long as we are supposed to be setting up a vhost
+if [ "$no_vhost" == "0" ]; then
+  # Prompt the user for the domain to use for the site
+  domain_name=
+  while [[ $domain_name = "" ]]; do
+    read -p "Enter the domain name (WITHOUT TLD) for this site: " domain_name
+  done
+fi
+
+
+##########################
+
+
+# # CREATE A DATABASE
+# create_database
+# # RUN DRUSH MAKE
+# drupal_download
+# # INSTALL DRUPAL
+# drupal_install
+# CREATE VHOST RECORD
+create_vhost
